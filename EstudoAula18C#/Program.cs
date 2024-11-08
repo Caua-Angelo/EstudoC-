88559705dc7b15520262f6e9e99e0bd3aa3b2538
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-08 07:15:03 -0300

    adicionando o estudos até agora

---
+++ EstudoAula18C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+namespace EstudoAula18C_ {
+    internal class Program {
+        static void Main(string[] args) {
+            int idade = 30;
+            double saldo = 10.56743;
+            string nome = "Maria";
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais",nome, idade, saldo);
+
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            Console.WriteLine(nome+ " tem " + idade + "anos  e tem saldo igual a "+ saldo.ToString("F2",CultureInfo.InvariantCulture) + " reais ");
+
+        }
+    }
+}
---
+++ EstudoAula18C#Exercicio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace EstudoAula18C_Exercicio {
+    internal class Program {
+        static void Main(string[] args) {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double preco3 = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine("{0}, cujo preço é $ {1:F2}", produto1, preco1);
+            Console.WriteLine("{0}, cujo preço é $ {1:F2}",produto2, preco2);
+
+            Console.WriteLine("Registro: {0} anos de idade, Código {1} e gênero : {2}",idade,codigo,genero);
+            Console.WriteLine(preco3.ToString("F8"));
+            Console.WriteLine(preco3.ToString("F3"));
+            Console.WriteLine(preco3.ToString("F3",CultureInfo.InvariantCulture));
+    
+        } 
+    }
+} 
