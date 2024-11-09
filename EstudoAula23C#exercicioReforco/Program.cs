cdfedc056c97290ca410d1c66ff51c9b10c388e9
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-09 16:36:24 -0300

    adicionando o código da aula 23 onde ensina um pouco mais sobre a entrada de dados e um exercício para treinar isso

---
+++ EstudoAula23C#exercicioReforco/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace EstudoAula23C_exercicioReforco {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com seu nome Completo: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int NumQuartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double PrecoProduto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            
+
+            Console.WriteLine("Entre com seu último nome,idade e altura (mesma linha)");
+            string[] vet = Console.ReadLine().Split();
+
+            string ultimonome = vet[0];
+            string idade = vet[1];
+            string altura = vet[2];
+
+            Console.WriteLine("Seu nome é: " + nome );
+            Console.WriteLine("Sua casa tem: " + NumQuartos + " quartos");
+            Console.WriteLine("O preço do produto é: " + PrecoProduto.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Seu ultimo nome é:" + ultimonome);
+            Console.WriteLine("Sua idade é: " + idade);
+            Console.WriteLine("sua altura é: " + altura);
+            
+        }
+    }
+}
---
+++ EstudoAulaC#23/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace EstudoAulaC_23 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n1 = int.Parse(Console.ReadLine());//converte um numero de string em um número inteiro
+            char ch = char.Parse(Console.ReadLine());//converte um char para string
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);//parse transforma o tipo,console.readline
+                                                                                       //entra algum dado e cultureinfo transforma o , em .
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString(CultureInfo.InvariantCulture));//converte para o número sair com o .
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
