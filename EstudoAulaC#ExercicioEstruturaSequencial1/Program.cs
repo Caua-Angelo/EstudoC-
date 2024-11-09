e49a3e266939bbe916a5a7f40bb0e6461d663247
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-09 17:43:58 -0300

    adicionando o primeiro exercicio de estrutura sequencial

---
+++ EstudoAulaC#ExercicioEstruturaSequencial1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EstudoAulaC_ExercicioEstruturaSequencial1 {
+    internal class Program {
+        static void Main(string[] args) {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int soma = num1 + num2;
+            Console.WriteLine(soma);
+        }
+    }
+}
