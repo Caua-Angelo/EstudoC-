0f2fc8349ae24f936d0bf5537c5f7192f2f1f98c
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-10 08:36:01 -0300

    adicionando o terceiro exercicio de estrutura sequencial

---
+++ EstudoAulaC#ExercicioEstruturaSequencial3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EstudoAulaC_ExercicioEstruturaSequencial3 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite 4 valores para ver a diferença entre eles");
+
+            int numero1 = int.Parse(Console.ReadLine());
+            int numero2 = int.Parse(Console.ReadLine());
+            int numero3 = int.Parse(Console.ReadLine());
+            int numero4 = int.Parse(Console.ReadLine());
+
+            int diferenca = numero1 * numero2 - (numero3 * numero4) ;
+            Console.WriteLine(diferenca);
+        }
+    }
+}
