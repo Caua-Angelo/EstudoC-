cd94aca7a992c20f9e50ce024ea322a151837f12
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-10 20:06:12 -0300

    Adicionando o quinto exercício de estrutura sequencial

---
+++ ExercicioEstruturaSequencial5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace ExercicioEstruturaSequencial5 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com o  código da peça, a quantidade,o valor unitário");
+            int codpeca1 = int.Parse(Console.ReadLine());
+
+            int Quantidadepecas1 = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double Valorpeca1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com o  código da peça, a quantidade,o valor unitário");
+            int codpeca2 = int.Parse(Console.ReadLine());
+
+            int Quantidadepecas2 = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double Valorpeca2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double soma = (Quantidadepecas1 * Valorpeca1) + (Quantidadepecas2 * Valorpeca2) ;
+            Console.WriteLine("Valor a pagar: R$ " + soma.ToString("f2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
