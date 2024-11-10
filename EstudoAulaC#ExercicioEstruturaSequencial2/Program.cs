97e0b363bea224e2536a500c673f88cd7884480d
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-10 07:57:11 -0300

    adicionando o segundo exercicio de estrutura sequencial

---
+++ EstudoAulaC#ExercicioEstruturaSequencial2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+namespace EstudoAulaC_ExercicioEstruturaSequencial2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite o valor do raio do circulo ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+            double areaRaio = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("A área do círculo é: " + areaRaio.ToString("f4",CultureInfo.InvariantCulture));
+        }
+    }
+}
