325ee44e78c61754c6bd6cd632b858cb828061f5
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-11 13:49:52 -0300

    Aula 27  estrutura condicional parte 1 calculo impar,par

---
+++ EstudoAulaC#27/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+
+namespace EstudoAulaC_27 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine();
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) {
+                Console.WriteLine("Par");
+            }
+            else {
+                Console.WriteLine("Impar");
+
+            }
+        }
+    }
+}
