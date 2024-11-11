b1d5bcf455bd1bc708915240d5f7244fc236d140
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-11 14:42:01 -0300

    enviando o segundo exercicio de estruturas condicionais

---
+++ EstudoAulaC#ExercicioEstruturaCondicional2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EstudoAulaC_ExercicioEstruturaCondicional2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre um número");
+
+            int numero = int.Parse(Console.ReadLine());
+            if (numero % 2 == 0) {
+                Console.WriteLine("Par");
+            }
+            else {
+                Console.WriteLine("Impar");
+            }
+        }
+    }
+}
