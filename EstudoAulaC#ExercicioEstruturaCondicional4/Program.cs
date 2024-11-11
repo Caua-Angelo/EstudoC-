9745927d66dac1f797243a82dd41806ba29b248c
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-11 16:23:37 -0300

    enviando exercicios 4 e 5 de estrutura condicional

---
+++ EstudoAulaC#ExercicioEstruturaCondicional4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EstudoAulaC_ExercicioEstruturaCondicional4 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre 2 valores");
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int horainicial = int.Parse(valores[0]);
+            int horafinal = int.Parse(valores[1]);
+
+
+            int duracao = 0;
+            if (horainicial < horafinal) { //Esse if verifica se a horainicial é menor que a horafinal. Ou seja, o caso em que o horário final ocorre no mesmo dia que o horário inicial. exemplo: Se a horainicial for 8 (8h da manhã) e a horafinal for 15 (3h da tarde), a duração será simplesmente a diferença entre as duas horas: 15 - 8 = 7.
+                duracao = horafinal - horainicial;
+            }
+            else {
+                duracao = 24 - horainicial + horafinal;
+            }
+            Console.WriteLine("O jogo durou: " + duracao + "Horas ");
+        }
+    }
+}
---
+++ EstudoAulaC#ExercicioEstruturaCondicional5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace EstudoAulaC_ExercicioEstruturaCondicional5 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite o código do item e a quantidade na mesma linha");
+            string[] numeros = Console.ReadLine().Split(' ');
+            int codproduto = int.Parse(numeros[0]);
+            int quantidade = int.Parse(numeros[1]);
+
+            double cachorroquente = 4.00;
+            double xSalada = 4.50;
+            double xBacon = 5.00;
+            double torradaSimples = 2.00;
+            double Refrigerante = 1.50;
+
+
+            double Valorfinal = 0;
+            if (codproduto == 1) {
+                Valorfinal = cachorroquente * quantidade;
+            }
+            else if (codproduto == 2) {
+                Valorfinal = xSalada * quantidade;
+            }
+            else if (codproduto == 3) {
+                Valorfinal = xBacon * quantidade;
+            }
+            else if (codproduto == 4) {
+                Valorfinal = torradaSimples * quantidade;
+            }
+            else if (codproduto == 5) {
+                Valorfinal = Refrigerante * quantidade;
+            }
+            Console.WriteLine("Total: R$ " + Valorfinal.ToString("f2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
