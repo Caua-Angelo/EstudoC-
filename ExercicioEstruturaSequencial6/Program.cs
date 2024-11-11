06a2cf59770bb9f70b0a50ef952f544b7359f38c
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-11 06:06:04 -0300

    Versão incompleta do quinto e ultimo exercício de estrutura sequencial

---
+++ ExercicioEstruturaSequencial6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace ExercicioEstruturaSequencial6 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite três valores:");
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double triangulo = (A * C) / 2;
+            Console.WriteLine("Triangulo: " + triangulo.ToString("f3", CultureInfo.InvariantCulture));
+
+            double PI = 3.14159;
+            double circulo = PI * (Math.Pow(C, 2));
+            Console.WriteLine("circulo: " + circulo.ToString("f3"));
+
+            Double trapezio = 
+        }
+    }
+}
