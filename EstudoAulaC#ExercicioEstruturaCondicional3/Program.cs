7f530ce497074ee56a6bccecf32b63b026517f39
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-11 15:13:19 -0300

    enviando exercicio 3 de estrutura condicional

---
+++ EstudoAulaC#ExercicioEstruturaCondicional3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EstudoAulaC_ExercicioEstruturaCondicional3 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com 2 valores:");
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 % num2 == 0 || num2 % num1 == 0) {
+                Console.WriteLine("são multiplos");
+            }
+            else  {
+                Console.WriteLine("Não são multiplos");
+            }
+        }
+    }
+}
