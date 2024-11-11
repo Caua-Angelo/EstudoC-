5a15af5fbc233e36b4e76ffa034178cb63f8b0f1
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-11 14:24:51 -0300

    enviando o primeiro exercício de estruturas condicionais

---
+++ EstudoAulaC#ExercicioEstruturaCondicional1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EstudoAulaC_ExercicioEstruturaCondicional1 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite um número:");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero < 0) {
+                Console.WriteLine("Negativo");
+            }
+            else {
+                Console.WriteLine("não negativo");
+            }
+        }
+    }
+}
