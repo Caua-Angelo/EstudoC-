11ad6598e10cc289bcc6b4d642f720a80c337a79
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-12 14:10:10 -0300

    código da aula 32 que ensina sobre o while usando um exemplo de calcular a raiz quadrada de um número e parar quando o número for negativo

---
+++ EstudoAula32C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace EstudoAula32C_ {
+
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite um número");
+            String[] vet = Console.ReadLine().Split(' ');
+                double num = double.Parse(vet[0], CultureInfo.InvariantCulture);
+
+            while (num >= 0.0) {
+
+                double raiz = Math.Sqrt(num);
+                Console.WriteLine("A raiz quadrada é :" + raiz.ToString("f3", CultureInfo.InvariantCulture));
+                Console.WriteLine("Digite outro número");
+                num = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Numero negativo!");
+        }
+    }
+}
