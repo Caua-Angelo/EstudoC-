bae6a930a43e5c22e9203551017b3ad7ea5e0dbc
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-12 16:19:50 -0300

    enviando primeiro exercicio de estrutura while,aqui acabei usando um if dentro do while,oque se mostrou bem útil

---
+++ EstudoAulaC#ExercicioWhile1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EstudoAulaC_ExercicioWhile1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre um valor");
+            int senha= 0;
+            while (senha != 2002) {
+                senha = int.Parse(Console.ReadLine());
+                if (senha != 2002) { 
+                Console.WriteLine("Senha invalida");
+            }
+            }
+            Console.WriteLine("Acesso permitido");
+        }
+    }
+}
