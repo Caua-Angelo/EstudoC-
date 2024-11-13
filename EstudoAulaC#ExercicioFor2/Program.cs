9d992ad8796173bb0e94a34dc4b2d52aa0d920a8
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-13 15:30:50 -0300

    segundo exercício de estrutura for//notar que quando for usar o for deve ser criado uma variável para ser apenas o contador(pelomenos com a informação atual que tenho)

---
+++ EstudoAulaC#ExercicioFor2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EstudoAulaC_ExercicioFor2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Quanto números você vai digitar?");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            int dentro = 0;
+            int fora = 0;
+
+            for (int i = 0; i < quantidade; i++) {
+                int numero = int.Parse(Console.ReadLine());
+                if (numero >= 10 && numero <= 20) {
+                    dentro++;
+                }
+                else {
+                    fora++;
+                }
+            }
+                Console.WriteLine(dentro+ " in");
+                Console.WriteLine(fora+ " out");
+        }
+    }
+}
