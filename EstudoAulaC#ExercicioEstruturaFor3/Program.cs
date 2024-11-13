d7e5047eb03b359788fef204e39a6207b8251a8c
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-13 16:28:09 -0300

    terceirdo exercicio de for + materiais para estudo em pdf

--- EstudoAulaC#ExercicioEstruturaFor3/Program.cs
+++ EstudoAulaC#ExercicioEstruturaFor3/Program.cs
@@ -1,7 +1,27 @@
-﻿namespace EstudoAulaC_ExercicioFor3 {
+﻿using System.Globalization;
+
+namespace EstudoAulaC_ExercicioFor3 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insira o numero de casos que virão a seguir");
+            int quantCasos = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            for (int i = 0; i < quantCasos; i++) {
+
+                string[] vet = Console.ReadLine().Split(' ');
+                if (vet.Length != 3) {
+                    Console.WriteLine("Entrada inválida. Por favor, insira três números separados por espaço.");
+                    continue;
+                }
+                double a = double.Parse(vet[0],CultureInfo.InvariantCulture);
+                double b = double.Parse(vet[1],CultureInfo.InvariantCulture);
+                double c = double.Parse(vet[2],CultureInfo.InvariantCulture);
+
+                double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;//os números recebidos são multiplicados por seu
+                                                                    //peso  e depois divididos pela soma dos pesos nesse caso 10
+                Console.WriteLine(media.ToString("f1",CultureInfo.InvariantCulture));
+            }
+
         }
     }
 }
