b73290e8eb452fb8b95cbc7f29c57fae4f54a82f
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-13 14:44:11 -0300

    Aula 34 onde fala sobre o for e da uma exemplo com código + o primeiro exercicio de for//tive que usar um if dentro do for para completar

---
+++ EstudoAulaC#34/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EstudoAulaC_34 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Quantos números inteiros você quer digitar?");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= numero; i++) {
+                Console.Write("Valor#{0} ",i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine("Soma igual a :" + soma);
+        }
+    }
+}
---
+++ EstudoAulaC#ExercicioFor1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EstudoAulaC_ExercicioFor1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite um valor");
+            int valor = int.Parse(Console.ReadLine());
+            int impares = 1;
+
+            for (int i = 1; i <= valor; i++) {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                
+
+                }
+            }
+        }
+    }
+}
