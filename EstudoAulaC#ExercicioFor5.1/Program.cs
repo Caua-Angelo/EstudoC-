88053a9b896ffaedad5ee0fa741c49317498e18a
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-14 15:47:04 -0300

    exercicio de For para calcular o fatorial de um número,o primeiro fiz usando uma função e outro fiz sem função

---
+++ EstudoAulaC#ExercicioFor5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EstudoAulaC_ExercicioFor5._1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("entre um número");
+
+            int valor = int.Parse(Console.ReadLine());
+
+            int fat = 1;
+            for (int i = 1; i <= valor; i++) {
+                fat = fat * i;
+            }
+                Console.WriteLine(fat);
+        }
+    }
+}
