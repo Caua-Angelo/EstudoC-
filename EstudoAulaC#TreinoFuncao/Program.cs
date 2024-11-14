d22675b69e0e04794010b2d40103377b45f02c41
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-14 15:46:18 -0300

    Treino 1 e 2 de função que pedi para a IA gerar

---
+++ EstudoAulaC#TreinoFuncao/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml;
+
+namespace EstudoAulaC_TreinoFuncao {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Insira 2 valores");
+            string[] numeros = Console.ReadLine().Split(' ');
+            int num1 = int.Parse(numeros[0]);
+            int num2 = int.Parse(numeros[1]);
+
+            Console.WriteLine(soma(num1,num2));
+            static int soma(int x, int y) {
+                return x + y;
+            }
+        }
+    }
+}
