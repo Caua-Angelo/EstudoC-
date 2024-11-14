22ef43045acb2b79aa82cbb674111a3ba6dc87d7
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-14 16:45:09 -0300

    aula 38 codigo incompleto

---
+++ EstudoAulaC#38/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EstudoAulaC_38 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com as medidas do triangulo X!");
+            string[] trianguloX = Console.ReadLine().Split(' ');
+            double xa = double.Parse(trianguloX[0]);
+            double xb = double.Parse(trianguloX[1]);
+            double xc = double.Parse(trianguloX[2]);
+
+            Console.WriteLine("Entre com as medidas do triangulo Y!");
+            string[] trianguloY = Console.ReadLine().Split(' ');
+            double Ya = double.Parse(trianguloX[0]);
+            double Yb = double.Parse(trianguloX[1]);
+            double Yc = double.Parse(trianguloX[2]);
+
+        }
+
+        static int CalcularAreaTriangulo(double x, double y, double z) {
+            double p = (x + y + z) / 2;
+            double Area = Math.Sqrt((p - x) * (p - y) * (p - y));
+
+
+        }
+    }
+}
