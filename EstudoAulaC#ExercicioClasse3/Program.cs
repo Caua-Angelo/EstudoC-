e92ed1a4f110b654622e48365833d27ca94e7b3c
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-17 20:59:39 -0300

    Exercício de classes com cálculo de retângulo

---
+++ EstudoAulaC#ExercicioClasse3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace EstudoAulaC_ExercicioClasse3 {
+    internal class Program {
+        static void Main(string[] args) {
+            Retangulo retangulo1 = new Retangulo();
+
+            Console.WriteLine("Seja bem vindo ao calculo do Retangulo");
+
+            Console.WriteLine("Insira A largura");
+            retangulo1.largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Insira A altura");
+            retangulo1.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double area = retangulo1.Area();
+            Console.WriteLine("Area: " + area.ToString("f2",CultureInfo.InvariantCulture));
+            double perimetro = retangulo1.perimetro();
+            Console.WriteLine("Perimetro: "+ perimetro.ToString("f2",CultureInfo.InvariantCulture));
+            double diagonal = retangulo1.diagonal();
+            Console.WriteLine("Diagonal: "+ diagonal.ToString("f2",CultureInfo.InvariantCulture));
+        }
+    }
+}
