f842ac7c1959307c30652062be121a4dd60a5973
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-17 18:01:10 -0300

    Aula 42 onde mostra como  “Configurar” para imprimir todos os atributos da classe quando ela for por exemplo chamada em um Console.Writeline();

---
+++ EstudoAulaC#42/Produto.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Globalization;
+
+namespace EstudoAulaC_42 {
+    internal class Produto {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade = Quantidade + quantidade;
+        }
+        public void RemoverProdutos(int quantidade) {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString() {
+            return Nome
+                + ", R$ "
+                + Preco.ToString("f2", CultureInfo.InvariantCulture)
+                + ","
+                + Quantidade
+                + " Unidades, Total: $"
+                + ValorTotalEmEstoque().ToString("f2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ EstudoAulaC#42/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace EstudoAulaC_42 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Produto produto = new Produto();
+            Console.WriteLine("Insira os dados do produto em estoque:");
+            Console.Write("nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("preco: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("quantidade: ");
+            produto.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto " + produto);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos((qte));
+            Console.WriteLine("Dados atualizados" + produto);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque");
+            int qtremove = int.Parse((Console.ReadLine()));
+            produto.RemoverProdutos((qtremove));
+            Console.WriteLine("Dados atualizados" + produto);
+
+        }
+    }
+}
