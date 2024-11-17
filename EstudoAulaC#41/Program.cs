ce28be89b9230c2411d16e0e1ce6c8a2f3eb9f92
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-17 15:45:49 -0300

    nova adaptação do código da aula 38 porém com um metodo sendo criado na classe para fazer o cálculo da area do triangulo

---
+++ EstudoAulaC#41/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+
+namespace EstudoAulaC_38 {
+    internal class Program {
+        static void Main(string[] args) {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triangulo X!");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triangulo Y!");
+
+
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double AreaX = x.AreaTriangulo();
+
+            Double AreaY = y.AreaTriangulo();
+
+            Console.WriteLine("area de X = " + AreaX.ToString("f4", CultureInfo.InvariantCulture));
+            Console.WriteLine("area de Y = " + AreaY.ToString("f4", CultureInfo.InvariantCulture));
+
+            if (AreaX > AreaY) {
+                Console.WriteLine("O triangulo X é o maior");
+            }
+            else if (AreaY > AreaX) {
+                Console.WriteLine("O triangulo Y é o maior");
+            }
+            else {
+                Console.WriteLine("A area de ambos é igual");
+
+            }
+        }
+    }
+}
