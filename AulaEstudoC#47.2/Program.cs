f9e31d4c29480e45bfc974212ac636e3fb4320e3
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-18 15:52:35 -0300

    Código da aula anterior,porém agora usando membros estáticos

---
+++ AulaEstudoC#47.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using AulaEstudoC_47;
+using System.Globalization;
+
+namespace EstudoAulaC_46 {
+    internal class Program {
+
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Entre com o valor do raio");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia:" + circ.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume:" + volume.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi " + Calculadora.Pi.ToString("f2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
