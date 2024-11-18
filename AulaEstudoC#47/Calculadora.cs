732d77b592b957e6091f95db4be5b5a80ee96d48
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-18 15:49:18 -0300

    Código da aula 47,onde mostra  uma mini calculadora porém usando membros instanciado

---
+++ AulaEstudoC#47/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace AulaEstudoC_47 {
+    internal class Calculadora {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double r) {
+            return 2.0 * Pi * r;
+        }
+        public double Volume(double r) {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
---
+++ AulaEstudoC#47/Program.cs
@@ -0,0 +1,23 @@
+﻿using AulaEstudoC_47;
+using System.Globalization;
+
+namespace EstudoAulaC_46 {
+    internal class Program {
+
+        static void Main(string[] args) {
+            Calculadora calc = new Calculadora();
+
+            Console.WriteLine("Entre com o valor do raio");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine("Circunferencia:" + circ.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume:" + volume.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi " + calc.Pi.ToString("f2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
