ebc06efc14108f62522ee7af202be4aa880c69c1
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-18 16:28:41 -0300

    Exercicio para treinar o uso de membros estáticos

---
+++ AulaEstudoC#ExercicioMembrosEstáticos1/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace AulaEstudoC_ExercicioMembrosEstáticos1 {
+    internal class ConversorDeMoeda {
+        static double iof = 0.06;
+        public static double CalculoFinal(double quantia,double cotacao) {
+        double valor = quantia * cotacao;
+
+            return valor + valor  * iof;
+        }
+    }
+}
---
+++ AulaEstudoC#ExercicioMembrosEstáticos1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace AulaEstudoC_ExercicioMembrosEstáticos1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual a cotação do dolar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantos dólares você vai comprar?");
+            double quantia = double.Parse(Console.ReadLine());
+
+            double resultado = ConversorDeMoeda.CalculoFinal(quantia, cotacao);
+
+            Console.WriteLine("Valor a ser pago = " + resultado.ToString("f2",CultureInfo.InvariantCulture));
+        }
+    }
+}
