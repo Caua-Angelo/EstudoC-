3cf13acc7d6d49bed1064dee6f4b18205eb3145e
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-18 11:16:23 -0300

    sinc

---
+++ EstudoAulaC#ExercicioClasse4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EstudoAulaC_ExercicioClasse4 {
+    internal class Program {
+        static void Main(string[] args) {
+            Funcionario funcionario1 = new Funcionario();
+
+            Console.WriteLine("Imprima os dados do funcionário");
+            Console.Write("Nome: ");
+            funcionario1.nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario1.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Imposto");
+            funcionario1.imposto = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
