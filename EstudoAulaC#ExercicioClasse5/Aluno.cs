9792bce2f1d9af00516b3be7f70c304647bd9ba9
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-18 15:19:06 -0300

    Quinto e ultimo exercicio de classes e métodos

---
+++ EstudoAulaC#ExercicioClasse5/Aluno.cs
@@ -0,0 +1,32 @@
+﻿
+namespace EstudoAulaC_ExercicioClasse5 {
+    internal class Aluno {
+        public double nota1;
+        public double nota2;
+        public double nota3;
+        public string nome;
+
+        public double NotaFinal() {
+            return (nota1 + nota2 + nota3);
+        }
+        public bool AprovadoOuReprovado() {
+            if (NotaFinal() >= 60) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        public double NotaRestante() {
+            if (AprovadoOuReprovado()) {
+                return 0.0;
+            }
+            else {
+                return 60 - NotaFinal();
+            }
+        }
+    }
+}
+
+
+
---
+++ EstudoAulaC#ExercicioClasse5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace EstudoAulaC_ExercicioClasse5 {
+    internal class Program {
+        static void Main(string[] args) {
+            Aluno aluno1 = new Aluno();
+
+            Console.Write("Qual o nome do aluno: ");
+            aluno1.nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno");
+            aluno1.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno1.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno1.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Nota final: " + aluno1.NotaFinal().ToString("F2"), CultureInfo.InvariantCulture);
+            if (aluno1.AprovadoOuReprovado()) {
+                Console.WriteLine("Aprovado");
+            }
+            else {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram " + aluno1.NotaRestante().ToString("f2")+ "Pontos");
+            }
+        }
+    }
+}
