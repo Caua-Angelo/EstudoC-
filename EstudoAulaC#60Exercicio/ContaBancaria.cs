6c6d26b8de82dc3812bb3dbadbc1c874d9affc2c
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-19 16:48:27 -0300

    Exercicio proposto na aula 60 incompleto

---
+++ EstudoAulaC#60Exercicio/ContaBancaria.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace EstudoAulaC_60Exercicio {
+    internal class ContaBancaria {
+
+        public int NumeroConta { get; set; }
+        public string NomeTitular { get; set; }
+        public double ValorConta { get; set; }
+
+        /*/public ContaBancaria(int numeroconta,string nometitular,double valorconta) {
+            NumeroConta = numeroconta;
+            NomeTitular = nometitular;
+            ValorConta = valorconta;
+        }*/
+        public double DepositoInicial(double x) {
+            return ValorConta += x; {
+
+
+            }
+        }
+
+        public void AumentarValorConta(double x) {
+            ValorConta += x;
+        }
+        public override string ToString() {
+            return "Conta " + NumeroConta + "," + "Titular" + NomeTitular + "," + "Saldo: " + ValorConta;
+        }
+
+
+    }
+}
---
+++ EstudoAulaC#60Exercicio/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EstudoAulaC_60Exercicio {
+    internal class Program {
+        static void Main(string[] args) {
+            ContaBancaria contabancaria = new ContaBancaria();
+            Console.WriteLine("Entre o número da conta");
+            contabancaria.NumeroConta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre o titular da conta");
+            contabancaria.NomeTitular = Console.ReadLine();
+            Console.WriteLine("Haverá deposito inicial? S/N"); 
+            QuerDepositoInicial = 
+           
+
+
+
+        }
+    }
+}
