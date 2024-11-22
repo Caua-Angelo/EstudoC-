988c5631c1b3e2dc66e8176456ded8bc3d391731
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-22 16:54:42 -0300

    código incompleto do exercicio de listas

---
+++ EstudoAulaC#Exerciciolista/Funcionarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoAulaC_Exerciciolista {
+    internal class Funcionarios {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double salary { get; set; }
+        public Funcionarios(int id, string name, double salary) { 
+            Id = id;
+            Name = name;
+            salary = salary;
+            }
+        public Funcionarios() { }
+        public double AumentarSalario(double x) {
+            return salary *= (x / 100);
+        }
+        public override string ToString() {
+            return "Id: " + Id + ", " + Name + ", " + salary;
+        }
+
+    }
+}
+    
+
+
---
+++ EstudoAulaC#Exerciciolista/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace EstudoAulaC_Exerciciolista {
+
+    internal class Program {
+        static void Main(string[] args) {
+            List<Funcionarios> listaFuncionarios = new List<Funcionarios>();
+            Funcionarios Funcionario1 = new Funcionarios();
+            Console.WriteLine("Quantos funcionários serão registrados?!");
+            int QuantFuncionarios = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < QuantFuncionarios; i++) {
+                Console.WriteLine("Funcionário #" + (1+ i));
+                Console.Write("id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                listaFuncionarios.Add(new Funcionarios(id, name, salary));
+                Console.WriteLine(listaFuncionarios);
+            }
+            Console.Write("Entre com o id do funcionário que deverá ter o salario aumentado");
+            int IdAumentarsalario = int.Parse(Console.ReadLine());
+
+            Funcionarios funcioID = listaFuncionarios.Find(x => x.Id == IdAumentarsalario);
+            if (funcioID != null) {
+                Console.WriteLine("Qual a porcentagem?: ");
+                double PorcentagemAumento = double.Parse(Console.ReadLine());
+                funcioID.AumentarSalario(PorcentagemAumento);
+            }
+            else {
+                Console.WriteLine("Esse Id não está vinculado a nenhum funcionário");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Lista atualizada de funcionário");
+            foreach (Funcionarios item in listaFuncionarios) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
