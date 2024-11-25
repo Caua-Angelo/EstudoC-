865edf769f0f853d855925bdd1a073464dda35aa
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-25 14:59:00 -0300

    Exercicio anterior comentado

--- EstudoAulaC#ExercicioMatrizes/Program.cs
+++ EstudoAulaC#ExercicioMatrizes/Program.cs
@@ -14,21 +14,21 @@
                     mat[i, j] = int.Parse(values[j]); //os campos i e j da matriz vão receber os valors armazenados no vetor values
                 }
             }
-            Console.WriteLine("Main diagonal: ");
-            for (int i = 0; i < n; i++) {
-                Console.Write(mat[i, i] + " ");
+            Console.WriteLine("Main diagonal: ");//mostra os valores na diagonal principal
+            for (int i = 0; i < n; i++) {//percorre as linhas da matriz
+                Console.Write(mat[i, i] + " ");//mostra a linha e a coluna da matriz equivalente a "i"
             }
             Console.WriteLine();
 
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (mat[i, j] < 0) {
-                        count++;
+            int count = 0;//cria uma variável para ser o contador de números negativos dentro da matriz
+            for (int i = 0; i < n; i++) {//percorre a linhas da matriz
+                for (int j = 0; j < n; j++) {//percorre as colunas da matriz
+                    if (mat[i, j] < 0) {//verifica se os números percorridos nas linhas e colunas são negativos
+                        count++; //se forem negativos o valor de count aumenta
                     }
                 }
             }
-                    Console.WriteLine("Negativdes numbers: " + count);
+                    Console.WriteLine("Negatives numbers: " + count); //expõe o valor de números negativos
         }
     }
 }
