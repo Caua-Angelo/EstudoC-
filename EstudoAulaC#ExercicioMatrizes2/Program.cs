694ec8589120270459c4d9c92eefc5e7228f84d0
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-25 15:43:44 -0300

    Exercicio de matrizes não finalizado

---
+++ EstudoAulaC#ExercicioMatrizes2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EstudoAulaC_ExercicioMatrizes2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre a quantidade de linhas"); 
+            int linhas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com a quantidade de Colunas");
+            int colunas = int.Parse(Console.ReadLine());
+
+            int[,]  mat =  new int[linhas, colunas];
+
+            for (int i = 0; i < ; i++)
+
+            Console.WriteLine("Entre o número que deve ser procurado na matriz");
+            int numeromatriz = int.Parse(Console.ReadLine());
+            for (int i = 0; i < linhas; i++) {
+                for (int j = 0; j < colunas; j++) {
+                    if (mat[i,j] == numeromatriz) {
+                        Console.WriteLine("Position: " + i + j );
+
+                    }
+                }
+            }
+        }
+    }
+}
