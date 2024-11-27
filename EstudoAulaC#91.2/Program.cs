8651da4843cd7482a89ea669165dfdc69b1dc7af
Cauã Angelo <cauanbbb@hotmail.com>
2024-11-27 13:50:38 -0300

    Segundo código da aula 91,mostrando um pouco sobre as funções do Datetime

---
+++ EstudoAulaC#91.2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EstudoAulaC_91._2 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            DateTime d = DateTime.Now;
+
+            DateTime d2 = d.AddDays(7);
+
+            DateTime d3 = d.AddMinutes(3);
+
+            TimeSpan t = d.Subtract(d2);
+
+
+
+            Console.WriteLine(d);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+
+        }
+    }
+}
