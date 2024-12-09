959c5d4cac98a834cf464fb22f03b9c6ea82e258
Cauã Angelo <cauanbbb@hotmail.com>
2024-12-09 16:36:37 -0300

    sinc

---
+++ EstudoAulaC#ExercicioMetodosAbstratos/Enums/Color.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoAulaC_ExercicioMetodosAbstratos.Enums
+{
+    internal class Color
+    {
+        enum colors : int
+        {
+            Black = 0,
+            blue = 1,
+            red = 2,
+
+        }
+    }
+}
---
+++ EstudoAulaC#ExercicioMetodosAbstratos/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EstudoAulaC_ExercicioMetodosAbstratos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of shapes: ");
+            int numbershapes = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numbershapes; i++)
+            {
+                Console.Write("Shape #"+i+ "Data:" );
+                Console.Write("Rectangle or Circle (r/c)? ");
+                string shape = Console.ReadLine();
+                if (shape == "r" || shape == "R")
+                {
+
+                }
+                else if(shape == "C" || shape == "c")
+                {
+
+
+                }
+
+            }
+        }
+    }
+}
