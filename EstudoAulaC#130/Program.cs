2402f47cdf242ae8f798204c2f18f095b7fe0c41
Cauã Angelo <cauanbbb@hotmail.com>
2024-12-12 13:58:46 -0300

    aula 130 sobre arquivos

---
+++ EstudoAulaC#130/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EstudoAulaC_130
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcepath = @"C:\Users\caua.lopes\Documents/blabla.txt";
+
+            string targetpath = @"C:\Users\caua.lopes\Documents/blabla2.txt";
+
+            try
+            {
+                FileInfo fileinfo = new FileInfo(sourcepath);
+                fileinfo.CopyTo(targetpath);
+                string[] lines = File.ReadAllLines(targetpath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+
+                }
+            }
+            catch (IOException ex)
+            {
+
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(ex.Message);
+
+            }
+
+        }
+    }
+}
