42be711757b1e17303bf3ba68532ae780a5a2f51
Cauã Angelo <cauanbbb@hotmail.com>
2024-12-16 13:52:51 -0300

    versão anterior do codigo sem interface

---
+++ EstudoAulaExerciciointerfaces/Entities/Invoice.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Globalization;
+
+namespace EstudoAulaExerciciointerfaces.Entities
+{
+    internal class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+
+
+        }
+        public override string ToString()
+        {
+            return "Basic payment: "
+                + BasicPayment
+                + "\nTax: "
+                + Tax
+                + "\nTotal payment: "
+                + TotalPayment;
+        }
+    }
+}
--- EstudoAulaExerciciointerfaces/Program.cs
+++ EstudoAulaExerciciointerfaces/Program.cs
@@ -6,17 +6,15 @@ namespace EstudoAulaExerciciointerfaces
     {
         static void Main(string[] args)
         {
-          
-
             Console.WriteLine("enter rental data");
             Console.Write("Car Model: ");
             string carModel = Console.ReadLine();
 
             Console.Write("Pickup (dd/MM/yyyy hh:mm)");
-            DateTime pickup = DateTime.Parse(Console.ReadLine());
+            DateTime start = DateTime.Parse(Console.ReadLine());
 
             Console.Write("Return (dd/MM/yyyy hh:mm)");
-            DateTime Return = DateTime.Parse(Console.ReadLine());
+            DateTime finish = DateTime.Parse(Console.ReadLine());
 
             Console.Write("Enter price per hour: ");
             double pricePerHour = double.Parse(Console.ReadLine());
@@ -24,7 +22,7 @@ namespace EstudoAulaExerciciointerfaces
             Console.Write("Enter price per day: ");
             double priceperDay = double.Parse(Console.ReadLine());
 
-            Rental rental = new Rental(carModel, pickup, Return, pricePerHour, priceperDay);
+            Rental rental = new Rental(carModel, start, finish, pricePerHour, priceperDay);
 
             rental.Finalpayment();
 
