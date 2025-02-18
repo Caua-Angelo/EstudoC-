6927d99bd50bfa9aee4dc286567d6e26a35dbb01
cauã Angelo <cauanbbb@hotmail.com>
2025-02-18 16:21:00 -0300

    sync

---
+++ CleanArchMvc/CleanArchMvcDomain/Entities/Category.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchMvcDomain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvcDomain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private  set; }
+        public string Name { get; private  set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+            
+        }  public Category(int id,string name)
+        {
+            DomainExceptionValidation.When(Id < 0, "INVALID ID");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),"Invalid name.Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,"Invalid name.too short,minimum 3 characteres");
+
+            Name = name;
+        }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvcDomain/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchMvcDomain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvcDomain.Entities
+{
+    public sealed class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private  set; }
+        public string Description { get; private  set; }
+        public decimal Price { get; private  set; }
+        public int Stock { get; private  set; }
+        public string Image { get; private  set; }
+
+        private void ValidateDomain(string name,string description,decimal price,int stock,string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+        }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
