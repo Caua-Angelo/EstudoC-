4e579b334575bedfe0cdca182d8869ad0a6d0f82
cauã Angelo <cauanbbb@hotmail.com>
2025-03-11 15:27:28 -0300

    tudo funcionando(eu acho)

--- CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Repositories;
+using CleanArchMvcDomain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMvc.Infra.IoC
 {
-    public static class  DependencyInjection
+    public static class DependencyInjection
     {
-        public static IserviceCollection AddInfraestructure(this IServiceCollection,IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDBContext>(options => options.UseNpgsql(configuration.GetConnectionString(
+                "DefaltConnection"
+            ), b => b.MigrationsAssembly(typeof(ApplicationDBContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
     }
 }
