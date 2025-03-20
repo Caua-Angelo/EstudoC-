cb710eef120708fca8d0250af88c4f12d11ae7b6
cauã Angelo <cauanbbb@hotmail.com>
2025-03-20 16:22:38 -0300

    aula 50 td ok

---
+++ CleanArchMvc/CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOS;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvcDomain.Entities;
+using CleanArchMvcDomain.Interfaces;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+
+        private readonly IMapper _mapper;
+        public ProductService(IMapper mapper, IProductRepository productRepository)
+        {
+            _productRepository = productRepository ??
+                throw new ArgumentNullException(nameof(productRepository));
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productsEntity = await _productRepository.GetProducts();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+            var productEntity = await _productRepository.GetbyId(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductCategory(int? id)
+        {
+            var productEntity = await _productRepository.GetProductCategoryAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task Add(ProductDTO productDto)
+        {
+            var productEntity = _mapper.Map<Product>(productDto);
+            await _productRepository.Create(productEntity);
+        }
+
+        public async Task Update(ProductDTO productDto)
+        {
+            var productEntity = _mapper.Map<Product>(productDto);
+            await _productRepository.Update(productEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var productEntity = _productRepository.GetbyId(id).Result;
+            await _productRepository.Remove(productEntity);
+
+        }
+    }
+}
